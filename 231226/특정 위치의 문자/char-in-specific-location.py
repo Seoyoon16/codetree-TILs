a592f51316cf08735334292fa951d92ecf3880b6
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2023-12-26 15:30:53 +0900

    Title: 특정 위치의 문자, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231226/특정 위치의 문자/char-in-specific-location.py
@@ -0,0 +1,29 @@
+# # 1
+# word = ['A', 'P', 'P', 'L', 'E']
+
+# # 해당 문자를 찾지 못했다면 -1
+# idx = -1
+
+# # 문자 탐색
+# for i, char in enumerate(word):
+#     if char == 'L':
+#         idx = i
+
+# # 문자가 존재하지 않는 경우
+# if idx == -1:
+#     print("not exist")
+# else:
+#     print(idx)
+
+# # 2
+# word = ['A', 'P', 'P', 'L', 'E']
+# print(word.index('L'))
+
+arr = ['L', 'E', 'B', 'R', 'O', 'S']
+letter = input()
+
+for i, char in enumerate(arr):
+    if char == letter:
+        print(i)
+    if letter not in arr:
+        print("None")
