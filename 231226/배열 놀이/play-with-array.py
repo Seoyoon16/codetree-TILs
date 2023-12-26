2314d6096894805aea46f71f6c105572d42c7d1c
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2023-12-26 16:48:11 +0900

    Title: 배열 놀이, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

--- 231226/배열 놀이/play-with-array.py
+++ 231226/배열 놀이/play-with-array.py
@@ -1,18 +1,22 @@
 n, q = tuple(map(int, input().split()))
 arr_n = list(map(int, input().split()))
+arr_q = []
 for i in range(q):
-    arr_q = list(map(int, input().split()))
+    arr = list(map(int, input().split()))
+    arr_q.append(arr)
 
-    if arr_q[0] == 1:
-        a = arr_q[1]
+for i in range(len(arr_q)):
+    if arr_q[i][0] == 1:
+        a = arr_q[i][1]
         print(arr_n[a-1])
-    elif arr_q[0] == 2:
-        a = arr_q[1]
+    elif arr_q[i][0] == 2:
+        a = arr_q[i][1]
         if a in arr_n:
             idx = arr_n.index(a)
             print(idx+1)            
         else: print('0')
     else:
-        a = arr_q[1]; b = arr_q[2]
+        a = arr_q[i][1]; b = arr_q[i][2]
         for i in range(a-1, b):
             print(arr_n[i], end=' ')
+        print()
