a1341c5ae23f149c9e6457dab8d61ee24fef7270
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2023-12-28 14:11:08 +0900

    Title: 배열의 평균, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231228/배열의 평균/ave-of-array.py
@@ -0,0 +1,25 @@
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+arr_avg = []
+for i in range(2):
+    rsum = 0
+    for j in range(4):
+        rsum += arr_2d[i][j]
+    ravg = rsum/4
+    arr_avg.append(rsum/4)
+    print(f"{ravg:.1f}", end=' ')
+print()
+for i in range(4):
+    cavg = 0
+    cavg = (arr_2d[0][i] + arr_2d[1][i]) / 2
+    arr_avg.append(cavg)
+    print(f"{cavg:.1f}", end=' ')
+print()
+asum = 0
+for elem in arr_avg:
+    asum += elem
+
+print(f"{asum/6:.1f}")
