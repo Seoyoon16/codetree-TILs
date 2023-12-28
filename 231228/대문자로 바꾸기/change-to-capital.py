68d7b61e93a999a417794af34d60bb81161d9d90
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2023-12-28 11:36:50 +0900

    Title: 대문자로 바꾸기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/대문자로 바꾸기/change-to-capital.py
+++ 231228/대문자로 바꾸기/change-to-capital.py
@@ -5,7 +5,9 @@ arr_2d = [
 
 for i in range(5):
     for j in range(3):
+        # arr_2d[i][j] = chr(ord(arr_2d[i][j]) + ord('A') - ord('a'))
         arr_2d[i][j] = arr_2d[i][j].upper()
+        # 바로 프린트 가능: print(arr_2d[i][j], end=" ")
 
 for row in arr_2d:
     for col in row:
