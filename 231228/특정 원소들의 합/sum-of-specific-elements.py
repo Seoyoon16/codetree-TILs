e332062a577ca43db26110daf787f0764862b8fa
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2023-12-28 14:43:03 +0900

    Title: 특정 원소들의 합, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231228/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,12 @@
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+sum_v = 0; k = 3
+for i in range(4):
+    for j in range(4):
+        if i+j > 3:
+            break;
+        sum_v += arr_2d[j+i][i]
+
+print(sum_v)
