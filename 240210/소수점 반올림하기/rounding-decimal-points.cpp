2d79f99f0089c1f16b38b78480f4d7e964acc856
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-10 00:42:05 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 25.352;
+    std::cout << std::fixed;
+    std::cout.precision(1);
+    std::cout << a;
+    return 0;
+}
