c2de5300bb6a05c4eeaf132ebe878be789937d8e
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-10 00:40:35 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240210/달에서 무게 구하기/weight-on-the-moon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int weight = 13; double moon_w = 0.165;
+    std::cout << std::fixed;
+    std::cout.precision(6);
+    std::cout << weight << " * " << moon_w << " = " << weight * moon_w;
+
+    return 0;
+}
