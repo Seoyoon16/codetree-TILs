1b1b96802d5d948a6882d66e8702c68807eb18a1
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-11 11:57:06 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240211/데이터 교환/exchange-data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 5, b = 6, c = 7;
+    int temp1, temp2;
+    temp1 = b; temp2 = c;
+    b = a;
+    c = temp1;
+    a = temp2;
+
+    std::cout << a << std::endl << b << std::endl << c;
+    
+    return 0;
+}
