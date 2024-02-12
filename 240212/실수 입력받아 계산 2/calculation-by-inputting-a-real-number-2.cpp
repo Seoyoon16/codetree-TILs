c5d24b4d2b63249c2d1db12c4a85346d6876a716
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-12 14:24:51 +0900

    Title: 실수 입력받아 계산 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    double a;
+    cin >> a;
+    cout << fixed;
+    cout.precision(2);
+    cout << a + 1.5;
+    return 0;
+}
