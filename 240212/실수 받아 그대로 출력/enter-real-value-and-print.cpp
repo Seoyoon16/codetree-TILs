e1a52c83986120721a54da912742232e517ecd3e
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-12 14:21:31 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    double n;
+    cin >> n;
+
+    cout << fixed;
+    cout.precision(2);
+    
+    cout << n;
+    return 0;
+}
