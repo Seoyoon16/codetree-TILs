f9ccbcb00b0bfa8f46f52ca242e062b2ba994c64
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-12 14:12:29 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240212/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a;
+    cin >> a;
+    cout << "Your score is " << a << " point.";
+    return 0;
+}
