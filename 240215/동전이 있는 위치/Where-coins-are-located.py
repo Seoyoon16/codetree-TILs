4c62133efef674115f90a686314e93233926f217
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-15 09:54:29 +0900

    Title: 동전이 있는 위치, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/동전이 있는 위치/Where-coins-are-located.py
+++ 240215/동전이 있는 위치/Where-coins-are-located.py
@@ -13,3 +13,31 @@ for i in range(1, n+1):
     for j in range(1, n+1):
         print(arr[i][j], end=' ')
     print()
+
+# # How to search a particular point
+
+# # the particular point that i try to search
+# a, b = 1, 3
+
+# # create a 10by10 2D array
+# placed = [
+#     [0 for _ in range(10)]
+#     for _ in range(10)
+# ]
+
+# # mark given points
+# for _ in range(3): # three is the number of points
+#     r, c = tuple(map(int, input().split()))
+#     placed[r][c] = 1
+
+# # print the array
+# for row in placed:
+#     for elem in row:
+#         print(elem, end=' ')
+#     print()
+
+# print()
+
+# # print result 
+# exists = True if placed[a][b] == 1 else False
+# print(exists)
