1a85a46be22c260699f684b2b2f01f8a85af23e1
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-19 09:22:44 +0900

    Title: 문자열 역순으로 출력하기, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240219/문자열 역순으로 출력하기/print-string-in-reverse.py
@@ -0,0 +1,19 @@
+arr = [
+    input()
+    for _ in range(4)
+]
+
+for i in range(3, -1, -1):
+    print(arr[i])
+
+
+
+
+
+
+
+
+# arr = []
+# for _ in range(4):
+#     given_str = input()
+#     arr.append(given_str)
