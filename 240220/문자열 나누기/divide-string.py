08769da278a2a1f7c6f14a1e564ba20e030edfda
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-20 09:22:23 +0900

    Title: 문자열 나누기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240220/문자열 나누기/divide-string.py
+++ 240220/문자열 나누기/divide-string.py
@@ -6,4 +6,9 @@ for s in nums:
     ts += s
 
 for i in range(0, len(ts), 5):
-    print(ts[i:i+5])
+    print(ts[i:i+5]) 
+
+# for i in range(leng):
+# 	print(str_all[i], end="")
+# 	if (i + 1) % 5 == 0:
+# 		print()
