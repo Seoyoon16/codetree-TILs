5a77c68cd49303756cf9f616a775389fc7b23d89
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-26 12:49:11 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b;
+    return 0;
+}
