0932661e906cf3f39b1c7c50c7b24f77ec39ab88
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-26 11:20:57 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240226/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int h, m;
+    cin >> h;
+    cin.get();
+    cin >> m;
+    cout << h+1 << ':' << m;
+
+    return 0;
+}
