b3e86a9eff55c78a1eb81f284aaf3d5b6d2c1b15
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-27 13:42:41 +0900

    Title: 숫자의 조건 여부, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240227/숫자의 조건 여부/number's-condition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    int a;
+    cin >> a;
+    if (a >= 113) {
+        cout << 1;
+    }
+    else {
+        cout << 0;
+    }
+    return 0;
+}
