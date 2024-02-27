9e58edb50b1e3e505a1835a17fbab5ddcf0110a4
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-02-27 13:16:17 +0900

    Title: 큰 수에서 빼기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240227/큰 수에서 빼기/subtract-from-large-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    int a, b;
+
+    cin >> a >> b;
+    if (a > b) {
+        cout << a - b;
+    }
+    if (b > a) {
+        cout << b - a;
+    }
+    
+    return 0;
+}
