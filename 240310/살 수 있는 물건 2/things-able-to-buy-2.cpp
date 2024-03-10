5c0d444b99405d116fc5b79c290a546bf8c1644f
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-10 23:37:10 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    if (n >= 3000) cout << "book";
+    else if (n >= 1000) cout << "mask";
+    else if (n >= 500) cout << "pen";
+    else cout << "no";
+    
+    return 0;
+}
