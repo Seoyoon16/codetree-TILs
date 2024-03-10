3def44a97c29a8b98a29b78759ac6c0a35da6426
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-10 23:32:40 +0900

    Title: 등급 매기기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240310/등급 매기기/ranking.cpp
+++ 240310/등급 매기기/ranking.cpp
@@ -10,8 +10,8 @@ int main() {
     if (score >= 90) cout << 'A';
     else if (score >= 80) cout << 'B';
     else if (score >= 70) cout << 'C';
-    else if (score >= 90) cout << 'D';
+    else if (score >= 60) cout << 'D';
     else cout << 'F';
-    
+
     return 0;
 }
