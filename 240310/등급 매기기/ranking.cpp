8c2c65838e17e472c190ecc9c6244d374b5932a5
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-10 23:32:22 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240310/등급 매기기/ranking.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int score;
+    cin >> score;
+
+    if (score >= 90) cout << 'A';
+    else if (score >= 80) cout << 'B';
+    else if (score >= 70) cout << 'C';
+    else if (score >= 90) cout << 'D';
+    else cout << 'F';
+    
+    return 0;
+}
