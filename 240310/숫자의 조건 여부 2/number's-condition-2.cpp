d049f5cea69771d616f804d320e991d63946057d
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-10 23:42:04 +0900

    Title: 숫자의 조건 여부 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240310/숫자의 조건 여부 2/number's-condition-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a;
+    cin >> a;
+
+    if (a == 5) cout << 'A';
+    if (a%2 == 0) cout << 'B';
+
+    return 0;
+}
