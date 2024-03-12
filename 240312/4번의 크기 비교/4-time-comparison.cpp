ee40122a1a854c80877b81aadcf9d91ecdd20bd4
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-12 22:07:03 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e);
+
+    return 0;
+}
