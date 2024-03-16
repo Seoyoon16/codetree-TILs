8d2733b76b3537a4bf4fe3936eded755beb43672
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-16 16:36:51 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240316/두 사람/two-person.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int age1, age2;
+    char sex1, sex2;
+    cin >> age1 >> sex1 >> age2 >> sex2;
+
+    if ((age1 >= 19 || age2 >= 19) && (sex1 == 'M' || sex2 == 'M')) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
