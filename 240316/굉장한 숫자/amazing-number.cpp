ab8c1d212e71dbee3a73ba20d6de6ddc4322fd50
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-16 16:10:24 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240316/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    int n;
+    cin >> n;
+
+    if ((n%2 == 1 && n%3 == 0) || (n%2 == 0 && n%5 == 0)) cout << "true";
+    else cout << "false";
+
+    return 0;
+}
