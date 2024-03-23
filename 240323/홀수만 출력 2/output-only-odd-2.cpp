1b6c83006027ca84ab0bbf38e72f1c9689d0a010
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-23 18:43:56 +0900

    Title: 홀수만 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240323/홀수만 출력 2/output-only-odd-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a, b;
+    cin >> a >> b;
+
+    for (int i=a; i>=b; i-=2) cout << i << ' ';
+
+    return 0;
+}
