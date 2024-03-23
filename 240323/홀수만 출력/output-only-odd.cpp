70da8280a156d6f09ae625d26ad73e742df59a1a
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-23 18:21:33 +0900

    Title: 홀수만 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240323/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a, b;
+    cin >> a >> b;
+
+    for (int i=1; i>=a && i <=b; i+=2) {
+        cout << i << ' ';
+    }
+    
+    return 0;
+}
