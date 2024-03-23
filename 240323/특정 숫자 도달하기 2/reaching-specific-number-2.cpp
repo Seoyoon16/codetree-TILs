2f8439fd590871948ef550dbdb3261986f656a7a
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-03-23 18:37:40 +0900

    Title: 특정 숫자 도달하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240323/특정 숫자 도달하기 2/reaching-specific-number-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    for (int i=n; i>0; i--) cout << i << ' ';
+    
+    return 0;
+}
