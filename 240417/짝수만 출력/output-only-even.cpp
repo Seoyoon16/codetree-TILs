36074e3eee84122755c14254cfa053d6c1d15716
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-04-17 23:31:31 +0900

    Title: 짝수만 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int a, b;
+    cin >> a >> b;
+
+    int i = a;
+    while (i <= b) {
+        cout << i << ' ';
+        i+=2;
+    }
+    return 0;
+}
