19cb8f721f7fc012061dcafdef3b850a9032be2f
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-04-17 23:33:11 +0900

    Title: 감소시키며 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/감소시키며 출력하기 2/decrease-and-print-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int i = 26;
+    while (i > 9) {
+        cout << i << ' ';
+        i--;
+    }
+    return 0;
+}
