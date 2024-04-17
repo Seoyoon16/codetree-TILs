52cd0d1966d4304d141ea62a7bd525eba3d64548
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-04-17 23:16:51 +0900

    Title: 별 n개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/별 n개 출력하기/print-n-stars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    int i = 0;
+    while (i<n) {
+        cout << '*' << endl;
+        i++;
+    }
+    return 0;
+}
