c3245da433bc1ac1e77f9757e85a4293f0fd53cb
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-04-17 23:34:11 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240417/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+    
+    int n;
+    cin >> n;
+
+    int i = n;
+    while (i > 0) {
+        cout << i << ' ';
+        i--;
+    }
+    return 0;
+}
