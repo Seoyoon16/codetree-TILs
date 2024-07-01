397f59bcd2e2de444a162f236fd0853c54aab5d1
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-07-02 03:58:12 +0900

    Title: 범위 안의 수 2, Time: 449ms, Memory: 32MB, Status: Wrong Answer - CodeTree

--- 240702/범위 안의 수 2/number-in-range-2.py
+++ 240702/범위 안의 수 2/number-in-range-2.py
@@ -1,10 +1,25 @@
-total = 0; number = 0
-for _ in range(10):
-    num = int(input())
-    if num >= 0 and num <= 200:
-        total += num
-        number += 1
+# # 1
+# total = 0; number = 0
+# for _ in range(10):
+#     num = int(input())
+#     if num >= 0 and num <= 200:
+#         total += num
+#         number += 1
 
-avg = round(total / number, 1)
+# avg = round(total / number, 1)
+
+# print(total, avg)
+
+# 2
+import statistics
+
+numbers = [int(input()) for _ in range(10)]
+
+for num in numbers:
+    if num < 0 or num > 200:
+        numbers.remove(num)
+
+total = sum(numbers)
+avg = round(statistics.mean(numbers), 1)
 
 print(total, avg)
