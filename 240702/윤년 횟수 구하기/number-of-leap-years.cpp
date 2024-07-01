759fd1beee5c933932f3e66aff8d99e391ab7f66
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-07-02 02:22:14 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240702/윤년 횟수 구하기/number-of-leap-years.cpp
+++ 240702/윤년 횟수 구하기/number-of-leap-years.cpp
@@ -9,6 +9,7 @@ int main() {
 
     for (int i=1; i<=n; i++) {
         if (i%4 != 0 || i%100 == 0 && i%400 != 0) day += 1;
+        // if((i % 4 == 0 && i % 100 != 0) || i % 400 == 0) leap++;
     }
     cout << n-day;
 
