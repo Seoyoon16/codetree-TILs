adaf701fd20f8e77c34223150c7e5c9dbf15c689
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-07-02 17:45:33 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240702/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (int i=1; i<=n; i++) {
+        if (i%2 == 0 || i%3 == 0 || i%5 == 0) cnt += 1;
+    }
+    cout << n - cnt;
+
+    return 0;
+}
