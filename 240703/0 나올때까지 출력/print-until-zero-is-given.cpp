ca350a07b9a40e7d76a2189da0e31b4cfb10b142
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-07-03 16:16:50 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240703/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    while (true) {
+        cin >> n;
+        if (n == 0) break;
+        cout << n << endl; 
+    }
+
+    return 0;
+}
