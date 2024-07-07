b8d735ac8cfb3b9ee527380f71f736482bdfc7a4
scarlet <59590562+Seoyoon16@users.noreply.github.com>
2024-07-07 22:53:09 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240707/조건에 따른 연산/operatino-by-rule.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    using namespace std;
+
+    int n;
+    cin >> n;
+
+    int cnt = 0;
+    for (;;) {
+        if (n%2 == 0) n = 3 * n + 1;
+        else n = 2 * n + 2;
+        cnt++;
+
+        if (n >= 1000) break;
+    }
+    cout << cnt;
+
+    return 0;
+}
